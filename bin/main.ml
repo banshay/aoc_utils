545a42d283c1f102aa750f103f608344b00b8773
Lukas Bopp <lukas@likemagic.tech>
2023-11-30 18:33:53 +0100

    Update

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,32 @@
-let () = print_endline "Hello, World!"
+let command = ref []
+
+let dir = ref "./"
+
+let command_spec_list =
+  [("-dir", Arg.Set_string dir, "Directory where to run the utils.")]
+
+let anon_fun c = command := c :: !command
+
+let () =
+  Arg.parse command_spec_list anon_fun "COMMAND is required" ;
+  match command.contents with
+  | "clear" :: rest -> (
+    match rest with
+    | lower :: upper :: _ ->
+        Lib.Setup.cleanup dir.contents (int_of_string lower)
+          (int_of_string upper)
+    | lower :: _ ->
+        Lib.Setup.cleanup dir.contents (int_of_string lower) 25
+    | [] ->
+        Lib.Setup.cleanup dir.contents 1 25 )
+  | "create" :: rest -> (
+    match rest with
+    | lower :: upper :: _ ->
+        Lib.Setup.create dir.contents (int_of_string lower)
+          (int_of_string upper)
+    | lower :: _ ->
+        Lib.Setup.create dir.contents (int_of_string lower) 25
+    | [] ->
+        Lib.Setup.create dir.contents 1 25 )
+  | _ ->
+      print_endline "Command not found!"
--- lib/setup/setup.ml
+++ lib/setup/setup.ml
@@ -1,3 +1,57 @@
 let rec range lower upper =
   if lower < upper then lower :: range (lower + 1) upper else [ upper ]
-;;
+
+let day_string number = Printf.sprintf "Day%02d" number
+
+let create_path dir name =
+  let trailing_slash = Str.regexp "/$" in
+  Printf.sprintf "%s/%s" (Str.global_replace trailing_slash "" dir) name
+
+let cleanup dir lower upper =
+  let delete_dir dir name =
+    [ create_path dir name ] |> FileUtil.rm ~recurse:true ~force:Force
+  in
+  List.map day_string @@ range lower upper |> List.iter (delete_dir dir)
+
+type file = { name : string; content : string }
+
+let rec create_files files =
+  match files with
+  | [] -> ()
+  | file :: rest ->
+      let out = open_out file.name in
+      output_string out file.content;
+      close_out out;
+      create_files rest
+
+let file_templates day =
+  [
+    {
+      name = create_path day "main.ml";
+      content = {|
+let () = print_endline "Hello World"
+  |};
+    };
+    { name = create_path day "input"; content = "" };
+    {
+      name = create_path day "dune";
+      content =
+        Printf.sprintf
+          {|
+(executable
+ (public_name %s)
+ (name main)
+ (libraries lib))
+  |}
+          day;
+    };
+  ]
+
+let create dir lower upper =
+  let create_dir dir name =
+    FileUtil.mkdir ~parent:true (create_path dir name)
+  in
+  List.map day_string @@ range lower upper
+  |> List.iter (fun day ->
+         create_dir dir day;
+         create_files @@ file_templates day)
